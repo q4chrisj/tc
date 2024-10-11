ca2c1360fcc5164478960e1ac357f2773f29f1a9
Chris Jones <chrisj@q4inc.com>
2024-10-11 09:53:47 -0400

    fix: don't need this service anymore

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 
 // https://blog.logrocket.com/building-typescript-cli-node-js-commander/
 
-import { SonarCloudRemovalService } from "./services/sonarcloud-removal.service";
 import { CLI } from "./ui/cli";
 
 async function run(): Promise<void> {
