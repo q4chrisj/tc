c49420e464fa51f2c072dd690b64fe648abfd91b
Chris Jones <chrisj@q4inc.com>
2024-10-12 15:05:29 -0400

    feat: pretty basic cache expiry

--- src/model/cache.ts
+++
@@ -1,5 +0,0 @@
-export type CachedData<T> = {
-  key: string;
-  expiration: number;
-  data: T;
-};
--- src/services/cache.service.ts
+++ src/services/cache.service.ts
@@ -1,6 +1,12 @@
 import * as fs from "node:fs/promises";
 import crypto from "crypto";
 
+type CachedData<T> = {
+  key: string;
+  expiration: number;
+  data: T;
+};
+
 export class CacheService {
   get = async <T>(key: string): Promise<T> => {
     const cacheKey = this._createCacheKey(key);
@@ -11,16 +17,27 @@ export class CacheService {
       return null as T;
     }
 
-    const cachedData = await fs.readFile(cacheEntry, "utf8");
-    const result: T = JSON.parse(cachedData);
-    return result;
+    const cacheString = await fs.readFile(cacheEntry, "utf8");
+    const result: CachedData<T> = JSON.parse(cacheString);
+    const now = Date.now();
+    if (result.expiration < now) {
+      console.log("Cache expired");
+      return null as T;
+    }
+    return result.data;
   };
 
-  set = async (key: string, value: any): Promise<void> => {
+  set = async <T>(key: string, value: T): Promise<void> => {
     const cacheKey = this._createCacheKey(key);
     const cacheEntry = `.cache/${cacheKey}.json`;
 
-    await fs.writeFile(cacheEntry, JSON.stringify(value), "utf8");
+    const cacheData: CachedData<T> = {
+      key: cacheKey,
+      expiration: Date.now() + 60 * 1000, // 1 minute
+      data: value,
+    };
+
+    await fs.writeFile(cacheEntry, JSON.stringify(cacheData), "utf8");
   };
 
   static init = async (): Promise<void> => {
@@ -34,15 +51,13 @@ export class CacheService {
     await fs.rm(".cache", { recursive: true }).catch(() => { });
   };
 
-  _ensureCacheDirectory = async (): Promise<void> => { };
-
-  _createCacheKey = (requestUri: string) => {
-    return crypto.createHash("md5").update(requestUri).digest("hex");
+  _createCacheKey = (cacheKeyString: string) => {
+    return crypto.createHash("md5").update(cacheKeyString).digest("hex");
   };
 }
-const _exists = async (cacheEntry: string): Promise<Boolean> => {
+const _exists = async (path: string): Promise<Boolean> => {
   return await fs
-    .access(cacheEntry, fs.constants.F_OK)
+    .access(path, fs.constants.F_OK)
     .then(() => {
       return true;
     })
